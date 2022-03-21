e498c6ce562fb68dcd188610282ef641dd939cd3
"Adetayo1999" <"adetayoomotomiwa99@gmail.com">
2022-03-21 12:47:23 +0100

    creating the whitlist contract

---
+++ contracts/whitelist.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Whitelist {
+    event whitelistLog(address, string);
+    uint maxWhitelistAddress = 3;
+    uint public currentlyWhitelisted;
+    mapping(address => bool) whitelistAddress;
+
+ 
+ function addToWhitelist() public {
+   require(!whitelistAddress[msg.sender], "Address already whitelisted");
+   require(currentlyWhitelisted < maxWhitelistAddress, "Whitelist full");
+   whitelistAddress[msg.sender] = true;
+   currentlyWhitelisted++;
+   emit whitelistLog(msg.sender, "Added to whitelist");
+ }
+
+ function getAddressStatus() public view returns(bool){
+   return whitelistAddress[msg.sender];
+ }
+
+  
+
+}
